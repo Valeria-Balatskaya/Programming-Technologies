651d65a99f80d3fcea439a401990f3b4eadae0cf
Valeriia Balatska <253850@edu.p.lodz.pl>
2025-04-14 20:18:08 +0200

    moved task1 implementation

---
+++ Task1/Library.Data/Entities/LibraryState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data.Entities
+{
+    public class LibraryState
+    {
+        public List<CatalogItem> AvailableBooks { get; } = new();
+        public List<CatalogItem> BorrowedBooks { get; } = new();
+    }
+}
